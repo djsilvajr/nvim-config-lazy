d102a887053a6d77ad76b6d12989d5b2e4d81a56
Douglas Junior <douglasjr0809@gmail.com>
2026-03-06 09:33:55 -0300

    Update Neovim configuration with new settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,22 +8,27 @@ vim.g.mapleader = " "
 -------------------------------------------------
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 vim.opt.termguicolors = true
 
 -------------------------------------------------
--- Bootstrap lazy.nvim
+-- Lazy bootstrap
 -------------------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
+    "https://github.com/folke/lazy.nvim",
+    "--branch=stable",
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 -------------------------------------------------
@@ -31,62 +36,63 @@ vim.opt.rtp:prepend(lazypath)
 -------------------------------------------------
 require("lazy").setup({
 
-  -- Mason
+  -------------------------------------------------
+  -- Colorscheme
+  -------------------------------------------------
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme tokyonight-night")
+    end
+  },
+
+  -------------------------------------------------
+  -- Treesitter (syntax highlight)
+  -------------------------------------------------
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
+    priority = 100,
+    opts = {
+      ensure_installed = { "lua", "vim", "vimdoc", "go" },
+      auto_install = true,
+      highlight = { enable = true }
+    }
+  },
+
+  -------------------------------------------------
+  -- LSP
+  -------------------------------------------------
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.config("gopls", {})
+      vim.lsp.enable("gopls")
+    end
+  },
+
   {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end,
+    end
   },
 
   {
     "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "phpactor", "lua_ls", "gopls" },
-        automatic_installation = true,
+        ensure_installed = { "gopls" }
       })
-    end,
+    end
   },
 
-  -- LSP (Neovim 0.11+)
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-
-      vim.lsp.start({
-        name = "phpactor",
-        cmd = { "phpactor" },
-        root_dir = vim.loop.cwd,
-      })
-
-      vim.lsp.start({
-        name = "lua_ls",
-        cmd = { "lua-language-server" },
-        root_dir = vim.loop.cwd,
-      })
-
-      -- Go LSP
-      vim.lsp.start({
-        name = "gopls",
-        cmd = { "gopls" },
-        root_dir = vim.loop.cwd,
-        settings = {
-          gopls = {
-            analyses = {
-              unusedparams = true,
-            },
-            staticcheck = true,
-            gofumpt = true,
-          },
-        },
-      })
-
-    end,
-  },
-
+  -------------------------------------------------
   -- Autocomplete
+  -------------------------------------------------
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
@@ -97,87 +103,66 @@ require("lazy").setup({
       local cmp = require("cmp")
       cmp.setup({
         sources = {
-          { name = "nvim_lsp" },
+          { name = "nvim_lsp" }
         },
         mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+        })
       })
-    end,
+    end
   },
 
-  -- Telescope
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-
-  -- File explorer
+  -------------------------------------------------
+  -- File tree
+  -------------------------------------------------
   {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("nvim-tree").setup()
-    end,
+    end
   },
 
+  -------------------------------------------------
+  -- Telescope
+  -------------------------------------------------
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+
+  -------------------------------------------------
+  -- Git worktree
+  -------------------------------------------------
+  {
+    "ThePrimeagen/git-worktree.nvim",
+    config = function()
+      require("git-worktree").setup()
+      require("telescope").load_extension("git_worktree")
+    end
+  },
+
+  -------------------------------------------------
   -- Git signs
+  -------------------------------------------------
   {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-    end,
+    end
   },
 
-  -- Git worktree
-  {
-    "ThePrimeagen/git-worktree.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    config = function()
-      require("git-worktree").setup()
-      require("telescope").load_extension("git_worktree")
-    end,
-  },
-
+  -------------------------------------------------
   -- Autopairs
+  -------------------------------------------------
   {
     "windwp/nvim-autopairs",
     config = function()
-      require("nvim-autopairs").setup({})
-    end,
-  },
+      require("nvim-autopairs").setup()
+    end
+  }
 
-  -- Treesitter (syntax highlighting)
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "php", "go", "bash", "json", "yaml", "markdown" },
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-      })
-    end,
-  },
-
-})
-
--------------------------------------------------
--- Folding (indent)
--------------------------------------------------
-vim.opt.foldenable = true
-vim.opt.foldlevelstart = 99
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "php", "blade" },
-  callback = function()
-    vim.opt_local.foldmethod = "indent"
-    vim.opt_local.foldlevel = 99
-  end,
 })
 
 -------------------------------------------------
@@ -185,21 +170,21 @@ vim.api.nvim_create_autocmd("FileType", {
 -------------------------------------------------
 
 -- File explorer
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { silent = true })
+vim.keymap.set("n", "<leader>e",  "<cmd>NvimTreeToggle<CR>",                                      { silent = true })
 
 -- Telescope
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { silent = true })
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>",                                { silent = true })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>",                                 { silent = true })
+vim.keymap.set("n", "<C-p>",      "<cmd>Telescope find_files<CR>",                                { silent = true })
 
 -- Git worktree
-vim.keymap.set("n", "<leader>gw", "<cmd>Telescope git_worktree git_worktrees<CR>", { silent = true })
-vim.keymap.set("n", "<leader>gn", "<cmd>Telescope git_worktree create_git_worktree<CR>", { silent = true })
-vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gw", "<cmd>Telescope git_worktree git_worktrees<CR>",                { silent = true })
+vim.keymap.set("n", "<leader>gn", "<cmd>Telescope git_worktree create_git_worktree<CR>",          { silent = true })
 
 -- LSP navigation
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "gr", vim.lsp.buf.references)
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "gd",         vim.lsp.buf.definition,                                         { silent = true })
+vim.keymap.set("n", "gr",         vim.lsp.buf.references,                                         { silent = true })
+vim.keymap.set("n", "K",          vim.lsp.buf.hover,                                              { silent = true })
 
 -- Folding toggle
-vim.keymap.set("n", "<leader>z", "za")
+vim.keymap.set("n", "<leader>z",  "za",                                                            { silent = true })
